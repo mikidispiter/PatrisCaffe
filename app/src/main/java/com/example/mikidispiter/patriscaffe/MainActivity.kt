c4fbba1af7531aedd54c2208de8483e27fc10edc
Miki Dispiter <mikidispiter@Mikis-MacBook-Air.local>
2018-03-24 08:52:34 +0100

    Initial commit
    Jesto len main aj prazdni meni activity

---
+++ app/src/main/java/com/example/mikidispiter/patriscaffe/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.mikidispiter.patriscaffe
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun Meni(view: View){
+        val OtvoriProzor2 = Intent(this, Meni::class.java)
+        startActivity(OtvoriProzor2)
+    }
+}
