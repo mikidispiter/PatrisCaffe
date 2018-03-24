8eff848159d47f18ea9249ffb295f070e43f071e
paveldispiter <pdispiter@yahoo.com>
2018-03-24 17:14:09 +0100

    1 deo

--- app/src/main/java/com/example/mikidispiter/patriscaffe/MainActivity.kt
+++ app/src/main/java/com/example/mikidispiter/patriscaffe/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mikidispiter.patriscaffe
 
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -10,10 +11,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
 
     fun Meni(view: View){
         val OtvoriProzor2 = Intent(this, Meni::class.java)
         startActivity(OtvoriProzor2)
     }
+
 }
--- app/src/main/java/com/example/mikidispiter/patriscaffe/Meni.kt
+++ app/src/main/java/com/example/mikidispiter/patriscaffe/Meni.kt
@@ -2,11 +2,17 @@ package com.example.mikidispiter.patriscaffe
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_meni.*
 
 class Meni : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_meni)
+    }
+
+    fun Turska(view: View){
+        textView3.text = "Prava domacinska besecera"
     }
 }
